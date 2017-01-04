b27f64944b703082ed5e9d6cfe55c858f374864e
Rasie1 <rasielll@gmail.com>
2017-01-04 13:31:24 +0300

    Add functional header to engine.h

--- include/Engine.h
+++ include/Engine.h
@@ -2,10 +2,11 @@
 
 #include "glm/glm.hpp"
 #include "base/System.h"
+#include <functional>
 
 namespace glowy3d
 {
 
-void start(std::function<void()> init, std::function<void()> update)
+void start(std::function<void()> init, std::function<void()> update);
 
 }
